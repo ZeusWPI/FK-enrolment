aa3af001d7e37799a071227c17ecbd3cf02d9f8f
Pieter De Baets <pieter.debaets@gmail.com>
2010-07-06 22:26:13 +0200

    Added index.php hiding

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ $config['base_url']	= "http://fk.local/";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = "index.php";
+$config['index_page'] = "";
 
 /*
 |--------------------------------------------------------------------------
