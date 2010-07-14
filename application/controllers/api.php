9a18546b7a4e265b769c0ee8bc69c19459ca83c3
Pieter De Baets <pieter.debaets@gmail.com>
2010-07-14 17:16:28 +0200

    Fix tabs vs. spaces, thx GilJ for feedback

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -4,37 +4,41 @@ require(APPPATH.'/libraries/REST_Controller.php');
 
 class Api extends REST_Controller
 {
-	function index_get() {
-		// @TODO: provide some info
-	}
+    function index_get() {
+        // @TODO: provide some info
+    }
 
-	function user_get()
+    function user_get()
     {
-		$this->load->model('Member');
+        $this->load->model('Member');
 
-		$member = new Member();
-		$member->kring_id = 554;
-		$member->first_name = 'Pieter';
-		$member->last_name = 'De Baets';
-		$member->email = 'pieter.debaets@ugent.be';
-		$member->ugent_nr = '00801234';
-		$member->validate();
-		var_dump($member->error);
+        $member = new Member();
+
+        /*$member->kring_id = 28;
+        $member->first_name = 'Pieter';
+        $member->last_name = 'De Baets';
+        $member->email = 'pieter.debaets@ugent.be';
+        $member->ugent_nr = '00801234';
+        $member->validate();
+        $member->save();
+        var_dump($member->error);*/
+
+        var_dump($member->get_by_id(1)->kring);
 
         /*if(!$this->get('id'))
         {
-        	$this->response(NULL, 400);
+            $this->response(NULL, 400);
         }
 
         // $user = $this->some_model->getSomething( $this->get('id') );
-    	$users = array(
-			1 => array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com', 'fact' => 'Loves swimming'),
-			2 => array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com', 'fact' => 'Has a huge face'),
-			3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => 'Is a Scott!'),
-		);
-		
-    	$user = @$users[$this->get('id')];
-    	
+        $users = array(
+            1 => array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com', 'fact' => 'Loves swimming'),
+            2 => array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com', 'fact' => 'Has a huge face'),
+            3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => 'Is a Scott!'),
+        );
+
+        $user = @$users[$this->get('id')];
+
         if($user)
         {
             $this->response($user, 200); // 200 being the HTTP response code
@@ -45,32 +49,32 @@ class Api extends REST_Controller
             $this->response(array('error' => 'User could not be found'), 404);
         }*/
     }
-    
+
     function user_post()
     {
         //$this->some_model->updateUser( $this->get('id') );
         $message = array('id' => $this->get('id'), 'name' => $this->post('name'), 'email' => $this->post('email'), 'message' => 'ADDED!');
-        
+
         $this->response($message, 200); // 200 being the HTTP response code
     }
-    
+
     function user_delete()
     {
-    	//$this->some_model->deletesomething( $this->get('id') );
+        //$this->some_model->deletesomething( $this->get('id') );
         $message = array('id' => $this->get('id'), 'message' => 'DELETED!');
-        
+
         $this->response($message, 200); // 200 being the HTTP response code
     }
-    
+
     function users_get()
     {
         //$users = $this->some_model->getSomething( $this->get('limit') );
         $users = array(
-			array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com'),
-			array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com'),
-			array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com'),
-		);
-        
+            array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com'),
+            array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com'),
+            array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com'),
+        );
+
         if($users)
         {
             $this->response($users, 200); // 200 being the HTTP response code
@@ -81,7 +85,7 @@ class Api extends REST_Controller
             $this->response(array('error' => 'Couldn\'t find any users!'), 404);
         }
     }
-    
+
 }
 
 ?>
