30e01ee917aa23c1e2464bef9bb7a314ce048155
Toon Willems <willemstoon@gmail.com>
2010-07-22 18:51:59 +0200

    hopefully this will give a better layout

--- application/views/register/via-ugentnr.php
+++ application/views/register/via-ugentnr.php
@@ -9,22 +9,22 @@
     <p>Vul onderstaand formulier in om je te registreren. Velden met een * zijn verplicht </p>
 <?php echo validation_errors() ?>
 <?php echo form_open('/registratie/via_ugentnr'); ?>
-    <?php echo form_label('* Geef uw stamnummer in:','stamnummer'); ?>
-    <?php echo form_input('stamnummer'); ?>
-    <?php echo form_label('* Geef uw voornaam in:','voornaam'); ?>
-    <?php echo form_input('voornaam'); ?>
-    <?php echo form_label('* Geef uw familienaam in:','familienaam'); ?>
-    <?php echo form_input('familienaam'); ?>
-    <?php echo form_label('Geef uw emailadres in:','emailadres'); ?>
-    <?php echo form_input('emailadres'); ?>
-    <?php echo form_label('Geef uw telefoonnummer in:','telefoonnummer'); ?>
-    <?php echo form_input('telefoonnummer'); ?>
-    <?php echo form_label('Geef uw kotadres in:','kotadres'); ?>
-    <?php echo form_input('kotadres'); ?>
-    <?php echo form_label('Geef uw thuisadres in:','thuisadres'); ?>
-    <?php echo form_input('thuisadres'); ?>
+    <p><?php echo form_label('* Geef uw stamnummer in:','stamnummer'); ?></p>
+    <p><?php echo form_input('stamnummer'); ?></p>
+    <p><?php echo form_label('* Geef uw voornaam in:','voornaam'); ?></p>
+    <p><?php echo form_input('voornaam'); ?></p>
+    <p><?php echo form_label('* Geef uw familienaam in:','familienaam'); ?></p>
+    <p><?php echo form_input('familienaam'); ?></p>
+    <p><?php echo form_label('Geef uw emailadres in:','emailadres'); ?></p>
+    <p><?php echo form_input('emailadres'); ?></p>
+    <p><?php echo form_label('Geef uw telefoonnummer in:','telefoonnummer'); ?></p>
+    <p><?php echo form_input('telefoonnummer'); ?></p>
+    <p><?php echo form_label('Geef uw kotadres in:','kotadres'); ?></p>
+    <p><?php echo form_input('kotadres'); ?></p>
+    <p><?php echo form_label('Geef uw thuisadres in:','thuisadres'); ?></p>
+    <p><?php echo form_input('thuisadres'); ?></p>
 
-    <?php echo form_submit('submit','Registreer'); ?>
+    <div><?php echo form_submit('submit','Registreer'); ?></div>
 <?php echo form_close(); ?>
     </div>
 </div>
