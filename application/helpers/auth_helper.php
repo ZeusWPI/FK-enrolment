35b080e15565b2bea087e7da3ceffea102bb66f4
Pieter De Baets <pieter.debaets@gmail.com>
2010-09-05 20:25:25 +0200

    Implemented FK auth

--- application/helpers/auth_helper.php
+++ application/helpers/auth_helper.php
@@ -8,10 +8,22 @@ function auth_user($sessionId) {
 
     $username = $db->query("SELECT username FROM phpbb3_sessions s
         LEFT JOIN phpbb3_users u ON u.user_id = s.session_user_id
-        WHERE s.session_id = ?", $sessionId)->row_array();
+        WHERE s.session_id = ?", array($sessionId))->row_array();
+    if(empty($username)) return -1;
 
-    // attempt 1
+    // attempt 1: username is user
+    $kring = $db->query("SELECT k.* FROM praesidia p
+        INNER JOIN kringen k ON p.kring = k.id
+        WHERE p.ugentlogin = ? AND p.jaar=? AND k.showonsite = 1",
+        array($username['username'], $CI->config->item('academic_year')))->row_array();
 
+    // attempt 2: username is kring
+    if(empty($kring)) {
+        $kring = $db->query("SELECT k.* FROM kringen k
+            WHERE k.kringname = ? AND k.showonsite = 1",
+            array($username['username']))->row_array();
+    }
 
-    //FKforum_sid > phpbb3_sessions > phpbb3_users > (via username) > praesidia > kringen
+    if(empty($kring)) return -1;
+    else return $kring['ID'];
 }
