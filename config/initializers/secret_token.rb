e7bfb347ef667c28f24683b619dda39ed46ebd06
Pieter De Baets <pieter.debaets@gmail.com>
2011-07-19 15:12:29 +0200

    `rails init .`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FKEnrolment::Application.config.secret_token = '30b849e50173610d039e3072826d9ef0ad680fa8d1ff5b8856deb2a4121bf98ec47bea6084dc2b492daedfed933834a026eab8b747b84a5316154c4cf7598a43'
