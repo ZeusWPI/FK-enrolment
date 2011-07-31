06cc0c319e68fcc3ae4d05110b556d53f4f911e1
Pieter De Baets <pieter.debaets@gmail.com>
2011-07-31 13:44:07 +0200

    Add warning to each page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :add_warning
+  def add_warning
+  	flash[:warning] = "De registratie-applicatie is nog niet gelanceerd!
+  		Alle registraties zullen bij lancering verwijderd worden."
+  end
 end
