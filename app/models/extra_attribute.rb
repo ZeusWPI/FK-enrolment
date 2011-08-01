f8fb853045f206df1ac59c22c807e13ad542f940
Toon Willems <willemstoon@gmail.com>
2011-08-01 13:40:52 +0200

    forgot to add values attribute

--- app/models/extra_attribute.rb
+++ app/models/extra_attribute.rb
@@ -1,4 +1,6 @@
 class ExtraAttribute < ActiveRecord::Base
   has_one :extra_attribute_spec 
   has_one :member
+
+  serialize :values
 end
--- app/models/extra_attribute_spec.rb
+++ app/models/extra_attribute_spec.rb
@@ -1,4 +1,6 @@
 class ExtraAttributeSpec < ActiveRecord::Base
   belongs_to :extra_attribute
   belongs_to :club
+
+  serialize :values
 end
