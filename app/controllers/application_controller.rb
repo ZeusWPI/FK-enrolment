f222421e88838a2d920c4e443374bbde736c8eb6
Pieter De Baets <pieter.debaets@gmail.com>
2011-08-01 11:38:51 +0200

    Go live

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :add_warning
-  def add_warning
-  	flash[:warning] = "De registratie-applicatie is nog niet gelanceerd!
-  		Alle registraties zullen bij lancering verwijderd worden."
-  end
 end
