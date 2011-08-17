0ce53a61da52fae54d61db9a91b56742ae01284a
Toon Willems <willemstoon@gmail.com>
2011-08-17 16:30:23 +0200

    Fix url and use new authentication

--- app/controllers/backend/backend_controller.rb
+++ app/controllers/backend/backend_controller.rb
@@ -14,9 +14,9 @@ class Backend::BackendController < ApplicationController
   # return which club this ugent_login is allowed to manage 
   def club_for_ugent_login(ugent_login)
     # using httparty because it is much easier to read than net/http code
-    resp = HTTParty.get('http.fkgent.be/api_zeus.php', 
+    resp = HTTParty.get('http://fkgent.be/api_zeus.php', 
                   :query => {
-                    :k => Rails::Application.config.zeus_api_key, 
+                    :k => digest(ugent_login, Rails::Application.config.zeus_api_key), 
                     :u => ugent_login
                   }
                 )
@@ -24,10 +24,14 @@ class Backend::BackendController < ApplicationController
     # this will only return the club name if controle hash matches 
     if resp.body != 'FAIL'
       hash = JSON[resp.body]
-      dig = Digest::SHA256.hexdigest Rails::Application.config.zeus_api_salt + '-' + ugent_login + hash['kringname']
+      dig = digest(Rails::Application.config.zeus_api_salt, ugent_login, hash['kringname'])
       return hash['kringname'] if hash['controle'] == dig
     end
     false
+  end
+
+  def digest(*args)
+    Digest::SHA256.hexdigest args.join('-')
   end
 
 end
