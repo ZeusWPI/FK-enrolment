69ec9294fffcf19d62de83f31f8dae708c9bdf76
Toon Willems <willemstoon@gmail.com>
2011-08-23 23:02:10 +0200

    Working ajax search

--- app/controllers/backend/members_controller.rb
+++ app/controllers/backend/members_controller.rb
@@ -1,9 +1,10 @@
 class Backend::MembersController < Backend::BackendController
 
-  respond_to :json, :xml
   def search
     @members = Member.where(:ugent_nr => params[:ugent_nr])
-    render :partial => "show", :locals => { :members => @members }, :layout => false, :status => :created
+    respond_to do |format|
+      format.js
+    end
   end
 
 end
