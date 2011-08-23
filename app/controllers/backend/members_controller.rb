1fd01fc6dfa3b6c496e2e58751cfa93a9dd0b296
Toon Willems <willemstoon@gmail.com>
2011-08-23 23:45:44 +0200

    Search now works for email, ugent_nr and name

--- app/controllers/backend/members_controller.rb
+++ app/controllers/backend/members_controller.rb
@@ -1,7 +1,14 @@
 class Backend::MembersController < Backend::BackendController
 
   def search
-    @members = Member.where(:ugent_nr => params[:ugent_nr])
+    case params[:search_field]
+    when 'ugent_nr'
+      @members = Member.where(:ugent_nr => params[:search_value])
+    when 'email'
+      @members = Member.where(:email => params[:search_value])
+    when 'name'
+      @members = Member.where('LOWER(first_name || " " || last_name) LIKE ?', "%#{params[:search_value].downcase}%")
+    end
     respond_to do |format|
       format.js
     end
