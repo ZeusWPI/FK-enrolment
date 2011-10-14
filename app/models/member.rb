1f5803209b26fa0dca1a27cd6064eb71f16ca046
Pieter De Baets <pieter.debaets@gmail.com>
2011-10-14 12:14:12 +0200

    Fix nil error

--- app/models/member.rb
+++ app/models/member.rb
@@ -137,8 +137,8 @@ class Member < ActiveRecord::Base
         card = member.current_card || Card.new
         sheet.row(i+1).concat [club.name, member.first_name, member.last_name,
           member.sex, member.ugent_nr, member.email, member.date_of_birth,
-          member.home_address.sub("\r\n", "\n"),
-          member.studenthome_address.sub("\r\n", "\n"),
+          member.home_address.try { |a| a.sub("\r\n", "\n") },
+          member.studenthome_address.try { |a| a.sub("\r\n", "\n") },
           member.photo.url(:cropped, use_timestamp = false), member.created_at,
           card.number, card.status, card.isic_status]
       end
