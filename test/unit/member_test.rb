272106975fa50e8a6ff1eb0c4e4d516fbc7a7df2
Toon Willems <willemstoon@gmail.com>
2012-10-02 18:03:10 +0200

    fix photo test

--- test/unit/member_test.rb
+++ test/unit/member_test.rb
@@ -5,7 +5,7 @@ class MemberTest < ActiveSupport::TestCase
   verify_fixtures Member
 
   def setup
-    @url = "http://placehold.it/210x270"
+    @url = "http://kelder.zeus.ugent.be/~nudded/fk-test.gif"
     File.open(File.join(fixture_path, "210x270.gif"), 'rb') { |f| @photo = f.read }
     @member = members(:javache)
   end
