0f79624b9ec56abaa8ea8cc30c05c96050de1e29
Pieter De Baets <pieter.debaets@gmail.com>
2013-08-22 10:57:34 +0200

    API works

--- lib/isic_export.rb
+++ lib/isic_export.rb
@@ -13,11 +13,15 @@ class IsicExport
   def initialize(wsdl_path)
     @client = Savon.client(
       wsdl: wsdl_path,
-      convert_request_keys_to: :camelcase
+      convert_request_keys_to: :none
     )
     @defaults = {
       username: Rails.application.config.isic_soap_user,
       password: Rails.application.config.isic_soap_password,
+
+      cardType: "ISIC",
+      StudentCity: "Gent",
+      School: "Universiteit Gent",
     }
   end
 
@@ -25,55 +29,56 @@ class IsicExport
     return if card.academic_year != Member.current_academic_year
     return if not ['request', 'revalidate'].include? card.isic_status
 
-    state_info {
-      'request': ['REQUESTED', 'requested'],
-      'revalidate': ['REVALIDATE', 'revalidated']
+    state_info = {
+      'request' => ['REQUESTED', 'requested'],
+      'revalidate' => ['REVALIDATE', 'revalidated']
     }
 
     params = @defaults.merge({
-      card_type: "ISIC",
-      student_city: "Gent",
-      school: "Universiteit Gent",
+      ClientID: "FK", # TODO: get client id from club
+      MemberNumber: card.number,
+      ISICCardNumber: card.isic_number, # TODO: what if empty?
+      Course: member.club.full_name,
+      type: state_info[card.isic_status][0],
 
-      client_id: "FK", # TODO: get client id from club
-      member_number: card.number,
-      isic_card_number: card.isic_number, # TODO: what if empty?
-      course: member.club.full_name,
-      type: state_info[card.isic_status][0]
-
-      first_name: member.first_name,
-      last_name: member.last_name,
-      birth_date: member.date_of_birth.strftime("%d/%m/%Y"),
-      birth_place: "",
-      gender: member.sex.upcase,
-      nationality: "",
+      Firstname: member.first_name,
+      Lastname: member.last_name,
+      BirthDate: member.date_of_birth.strftime("%d/%m/%Y"),
+      BirthPlace: "",
+      Gender: member.sex.upcase,
+      Nationality: "",
       language: "NL",
 
-      street: member.home_street,
-      postal_code: member.home_postal_code,
-      city: member.home_city,
+      Street: member.home_street,
+      PostalCode: member.home_postal_code,
+      City: member.home_city,
       email: member.email,
-      phone_number: member.phone,
-  
-      # TODO: crop correctly according to spec
-      # photo: ActiveSupport::Base64.encode64(open(member.photo_file_name).string),
-      # image_extension: "jpg",
+      PhoneNumber: member.phone,
 
-      is_student: "1",
-      year: "1", # TODO: check if this a good default
-      send_to_home: member.isic_mail_card ? "1" : "0",
-      optin: member.isic_newsletter ? "1" : "0",
-      promotion_code: "",
-      optin_third: "0",
-      special: "1",
+      isStudent: "1",
+      Year: "1", # TODO: check if this a good default
+      sendToHome: member.isic_mail_card ? "1" : "0",
+      promotionCode: "",
+      Optin: member.isic_newsletter ? "1" : "0",
+      optinThird: "0",
+      special: "1"
     })
 
-    client.call(:add_isic_registration, message: params)
+    photo = File.read(member.photo.path(:cropped), mode: 'rb')
+    params.update({
+      Photo: ActiveSupport::Base64.encode64(photo),
+      ImageExtension: "jpg"
+    })
 
-    # Update card state
-    card.isic_status = state_info[card.isic_status][1]
-    card.save
-  rescue Savon::Error => error
-    Logger.log error
-  end
+    response = @client.call(:add_isic_registration, message: params)
+    result = response.body[:add_isic_registration_response][:add_isic_registration_result]
+
+    if result[0] == 'S'
+      # Update card state
+      card.isic_status = state_info[card.isic_status][1]
+      card.isic_number = result
+      card.save
+    else
+      raise result
+    end
 end
