d23564ec58b32d4deda55b381ab0f61e21442616
Pieter De Baets <pieter.debaets@gmail.com>
2014-03-30 19:16:44 +0100

    ruby-saml changes

--- app/controllers/frontend/eid_controller.rb
+++ app/controllers/frontend/eid_controller.rb
@@ -3,7 +3,7 @@ class Frontend::EidController < Frontend::FrontendController
   def auth
     save_redirect :post_eid_redirect
 
-    request = Onelogin::Saml::Authrequest.new
+    request = ::OneLogin::RubySaml::Authrequest.new
     redirect_to request.create(saml_settings)
   end
 
@@ -18,7 +18,7 @@ class Frontend::EidController < Frontend::FrontendController
 
   skip_before_filter :verify_authenticity_token, :only => :receive
   def receive
-    result = Onelogin::Saml::Response.new(params[:SAMLResponse])
+    result = ::OneLogin::RubySaml::Response.new(params[:SAMLResponse])
     session[:eid] = result.attributes
 
     redirect_to session.delete(:post_eid_redirect) || root_url
@@ -32,7 +32,7 @@ class Frontend::EidController < Frontend::FrontendController
   private
 
   def saml_settings
-    Onelogin::Saml::Settings.new.tap do |s|
+    ::OneLogin::RubySaml::Settings.new.tap do |s|
       s.idp_sso_target_url             = "https://www.e-contract.be/eid-idp/protocol/saml2/post/ident"
       s.idp_cert_fingerprint           = "6ce7a376a1394a1be3585536685d7fdc7da19fb6"
       s.assertion_consumer_service_url = eid_receive_url
