bc6f0d6a423388a0e6642516deeae2e344b334e4
Tom Naessens <Tom.Naessens@UGent.be>
2015-07-07 22:03:06 +0200

    Third round of errors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-FKEnrolment::Application.config.session_store :active_record_store
+FKEnrolment::Application.config.session_store :cookie_store, key: '_Enrolment_session'
