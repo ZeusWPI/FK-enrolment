fe2d333d9668c6991c9e3a54a6f14ede585f0c75
Tom Naessens <Tom.Naessens@UGent.be>
2015-07-16 21:03:07 +0200

    Alter some tests

--- test/controllers/frontend/cas_controller_test.rb
+++ test/controllers/frontend/cas_controller_test.rb
@@ -1,24 +1,16 @@
 require 'test_helper'
 
 class Frontend::CasControllerTest < ActionController::TestCase
-  setup do
-    RubyCAS::Filter.fake(nil)
-  end
-
-  test "should auth user" do
+  test "should unauthorize not logged in users" do
     get :auth, :redirect => "/target"
     assert_response :redirect
 
-    RubyCAS::Filter.fake("pdbaets")
     get :verify
-    assert_response :redirect
-    assert_redirected_to "/target"
-    assert_equal "pdbaets", session[:cas_user]
+    assert_response :unauthorized
   end
 
-  test "should perform single-sign-out" do
-    params =  {"logoutRequest" => "<samlp:LogoutRequest xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" ID=\"LR-25247-PCqWKmp7cbsqX3s9eGmEgdeGwj4gXqoRvW5\" Version=\"2.0\" IssueInstant=\"2011-08-10T21:01:30Z\"><saml:NameID xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">@NOT_USED@</saml:NameID><samlp:SessionIndex>ST-149260-zfZ3mHobRXRrwQkSWmeL-cas</samlp:SessionIndex></samlp:LogoutRequest>"}
-    post :verify, params
-    assert_response :success
+  test "should log user out" do
+    get :logout
+    assert_redirected_to logout_path(url: root_url)
   end
 end
