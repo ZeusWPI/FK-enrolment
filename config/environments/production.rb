c9c4a71f57cbd5edd064f831cc72561443ce952c
Tom Naessens <Tom.Naessens@UGent.be>
2015-07-29 22:19:29 +0200

    Fix stuff by removing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,9 @@ FKEnrolment::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
+
+  config.log_level = :info
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -31,9 +33,6 @@ FKEnrolment::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
-  # Defaults to Rails.root.join("public/assets")
-  config.assets.manifest = Rails.root.join("public/assets")
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for nginx
