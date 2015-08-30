d630d5ff53d21bb552b8f1a81c9fafb8ebc6d054
Ilion Beyst <ilion.beyst@gmail.com>
2015-08-30 12:56:55 +0200

    fixed migration order

--- db/migrate/20150814153347_add_card_type_to_card.rb
+++ db/migrate/20150814153347_add_card_type_to_card.rb
@@ -1,8 +1,16 @@
 class AddCardTypeToCard < ActiveRecord::Migration
+  def fix_isic_card_number card
+    return if !card.isic? || card.academic_year != Member.current_academic_year
+    if !card.club.card_range_for(:isic).include?(card.number)
+      card.number = nil
+    end
+  end
+
   def change
     add_column :cards, :card_type, :string
     Card.find_each do |card|
       card.card_type = card.isic_status == 'none' ? 'fk' : 'isic'
+      fix_isic_card_number card
       card.save!
     end
     change_column :cards, :card_type, :text, null: false
