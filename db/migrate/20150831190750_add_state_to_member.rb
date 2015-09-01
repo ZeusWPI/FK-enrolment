4f2e0f13937d45d4d52ec1be9de9f85887249805
Tom Naessens <Tom.Naessens@UGent.be>
2015-09-01 23:45:19 +0200

    Made it another lightyear faster by deleting code

--- db/migrate/20150831190750_add_state_to_member.rb
+++ db/migrate/20150831190750_add_state_to_member.rb
@@ -3,9 +3,5 @@ class AddStateToMember < ActiveRecord::Migration
     # Use complete as the default state, since the other states are only
     # useful for the frontend registration.
     add_column :members, :state, :string, default: 'complete', null: false
-    Member.find_each do |member|
-      member.state = 'complete'
-      member.save(validate: false)
-    end
   end
 end
