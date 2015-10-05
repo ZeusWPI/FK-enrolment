35b0974d38ec8e9818a1e271e375fcecff509138
David Vandorpe <David.Vandorpe@UGent.be>
2015-10-05 20:47:06 +0200

    Fixed over-indentation

--- app/controllers/backend/backend_controller.rb
+++ app/controllers/backend/backend_controller.rb
@@ -47,9 +47,9 @@ class Backend::BackendController < ApplicationController
 
     # using httparty because it is much easier to read than net/http code
     resp = HTTParty.get(Rails.application.secrets.fk_auth_url, :query => {
-                                                                 :k => digest(ugent_login, Rails.application.secrets.fk_auth_key),
-                                                                 :u => ugent_login
-                                                             })
+             :k => digest(ugent_login, Rails.application.secrets.fk_auth_key),
+             :u => ugent_login
+         })
 
     # this will only return the club names if control-hash matches
     if resp.body != 'FAIL'
