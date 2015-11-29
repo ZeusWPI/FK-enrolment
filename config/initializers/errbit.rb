dcafd46dbeb57b0b139d968e29ecad8ac6329a48
Tom Naessens <Tom.Naessens@UGent.be>
2015-11-29 15:17:21 +0100

    Fix errbit host

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = '5b0914cc7a081f32f71f15a5696ca463'
-  config.host    = 'zeus-errbit.ilion.me'
+  config.host    = 'errbit.awesomepeople.tv'
   config.port    = 80
   config.secure  = config.port == 443
 end
