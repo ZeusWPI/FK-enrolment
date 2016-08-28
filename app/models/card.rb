3311e0af58d78eff8272bf8d81f763fca5bfbee0
David Vandorpe <david.vandorpe@ugent.be>
2016-08-28 20:56:21 +0200

    ALlow citylife card type

--- app/models/card.rb
+++ app/models/card.rb
@@ -32,7 +32,7 @@ class Card < ActiveRecord::Base
   # Validation rules
   validates :academic_year, :presence => true, :uniqueness => { :scope => :member_id }
   validates :number, :presence => true, :uniqueness => { :scope => :academic_year }
-  validates :card_type, :presence => true, :inclusion => { :in => %w(fk isic) }
+  validates :card_type, :presence => true, :inclusion => { :in => %w(fk isic citylife) }
   validates :status, :inclusion => { :in => %w(unpaid paid) }
   validates :isic_status, :inclusion => { :in => %w(none request requested printed) }
 
