fca31950b16dac1385ccca57403bb7c42b813b23
David Vandorpe <david.vandorpe@ugent.be>
2017-07-27 20:10:42 +0200

    Update FK api

--- app/controllers/backend/backend_controller.rb
+++ app/controllers/backend/backend_controller.rb
@@ -6,7 +6,8 @@ class Backend::BackendController < ApplicationController
   before_filter :verify_cas, :verify_club
 
   def verify_cas
-    if session[:cas].blank? && !Rails.env.development?
+    puts :test
+    if session[:cas].blank? #&& !Rails.env.development?
       redirect_to cas_auth_path(:redirect => request.fullpath)
     end
   end
@@ -37,24 +38,31 @@ class Backend::BackendController < ApplicationController
   private
   def request_clubnames
     # Development backdoor
-    return Club.pluck(:internal_name) if Rails.env.development?
+    #return Club.pluck(:internal_name) if Rails.env.development?
 
     ugent_login = session[:cas]['user']
+
     def digest(*args)
       Digest::SHA256.hexdigest args.join('-')
     end
 
     # using httparty because it is much easier to read than net/http code
-    resp = HTTParty.get(Rails.application.secrets.fk_auth_url, :query => {
-             :k => digest(ugent_login, Rails.application.secrets.fk_auth_key),
-             :u => ugent_login
-         })
+    resp = HTTParty.get("#{ Rails.application.secrets.fk_auth_url }/#{ ugent_login }/FKEnrolment",
+                        headers: {
+                            'X-Authorization': Rails.application.secrets.fk_auth_key,
+                            Accept: 'application/json'
+                        })
 
     # this will only return the club names if control-hash matches
-    if resp.body != 'FAIL'
+    if resp.success?
       hash = JSON[resp.body]
-      dig = digest(Rails.application.secrets.fk_auth_salt, ugent_login, hash['clubnames'])
-      return hash['clubnames'] if hash['control'] == dig
+      clubs = hash['clubs'].map do |club| club['internal_name'] end
+      timestamp = hash['timestamp']
+
+      return [] unless (Time.now - DateTime.parse(timestamp)).abs < 300 # Timestamp can't differ by more than 5 minutes
+      dig = digest(Rails.application.secrets.fk_auth_salt, ugent_login, timestamp, clubs)
+      byebug
+      return clubs if hash['sign'] == dig
     end
     []
   end
